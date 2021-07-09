8d734619e39bf55131185c6f351da38c9df71a8d
Mr.Rezoo <rezam578@gmail.com>
2021-07-09 14:42:22 +0430

    Build Logic of database conection and migration creation

--- migrations.php
+++ migrations.php
@@ -2,4 +2,27 @@
 
 
 
+use app\core\Application;
 
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
+
+$app = new Application(__DIR__, $config);
+
+$app->db->applyMigrations();
+
+
+//"MrRezoo\\PhpmvcFramework\\": "src/"
--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -3,12 +3,24 @@
 class m0001_initial {
     public function up()
     {
-        echo "applying migration";
+       $db = \app\core\Application::$app->db;
+       $SQL = " CREATE TABLE users (
+            id INT AUTO_INCREMENT PRIMARY KEY ,
+            email VARCHAR (255) NOT NULL ,
+            first_name VARCHAR (255) NOT NULL ,
+            last_name VARCHAR (255) NOT NULL ,
+            status TINYINT NOT NULL ,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+        ) ENGINE=INNODB;";
+       $db->pdo->exec($SQL);
+
     }
 
 
     public function down()
     {
-        echo "Down migration";
+        $db = \app\core\Application::$app->db;
+        $SQL = " DROP TABLE users;";
+        $db->pdo->exec($SQL);
     }
 }
